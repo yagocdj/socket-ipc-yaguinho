1aebc942abdd7a33aeff733e250342e0140cdac4
Gustavo Wagner Diniz Mendes <gugawag@gmail.com>
2016-03-06 02:51:52 -0300

    Arquivos client e server.

---
+++ src/com/gugawag/so/ipc/DateClient.java
@@ -0,0 +1,36 @@
+package com.gugawag.so.ipc;
+
+/**
+ * Client program requesting current date from server.
+ *
+ * Figure 3.22
+ *
+ * @author Silberschatz, Gagne and Galvin. Pequenas alterações feitas por Gustavo Wagner (gugawag@gmail.com)
+ * Operating System Concepts  - Eighth Edition
+ */ 
+
+import java.net.*;
+import java.io.*;
+
+public class DateClient
+{
+	public static void main(String[] args)  {
+		try {
+			// this could be changed to an IP name or address other than the localhost
+			Socket sock = new Socket("127.0.0.1",6013);
+			InputStream in = sock.getInputStream();
+			BufferedReader bin = new BufferedReader(new InputStreamReader(in));
+
+			System.out.println("=== Cliente iniciado ===");
+
+			String line;
+			while( (line = bin.readLine()) != null)
+				System.out.println("O servidor me disse:" + line);
+				
+			sock.close();
+		}
+		catch (IOException ioe) {
+				System.err.println(ioe);
+		}
+	}
+}
