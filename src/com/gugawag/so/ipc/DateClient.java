c9f5bdf8e20d3700bd1eedc3f38ef9bce5c698a3
Yago César <98711805+yagocdj@users.noreply.github.com>
2024-11-28 15:50:12 -0300

    Update DateClient.java with a sleep and a my friend's PC IPv4 address

--- src/com/gugawag/so/ipc/DateClient.java
+++ src/com/gugawag/so/ipc/DateClient.java
@@ -16,19 +16,22 @@ public class DateClient {
 	public static void main(String[] args)  {
 		try {
 			// this could be changed to an IP name or address other than the localhost
-			Socket sock = new Socket("localhost",6013);
+			Socket sock = new Socket("10.0.72.68",6013);
+
 			InputStream in = sock.getInputStream();
 			BufferedReader bin = new BufferedReader(new InputStreamReader(in));
 
 			System.out.println("=== Cliente iniciado ===\n");
-
+			
 			String line = bin.readLine();
 			System.out.println("O servidor me disse:" + line);
+			
+			Thread.sleep(50000);
 				
 			sock.close();
 		}
-		catch (IOException ioe) {
-				System.err.println(ioe);
+		catch (Exception ex) {
+			System.err.println(ex);
 		}
 	}
 }
