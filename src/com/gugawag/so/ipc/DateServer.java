68fa18dd80d5e4e01d47378ff24c9cc5bd979291
Yago César <98711805+yagocdj@users.noreply.github.com>
2024-11-28 15:45:50 -0300

    feat: Update DateServer.java with multiclient

--- src/com/gugawag/so/ipc/DateServer.java
+++ src/com/gugawag/so/ipc/DateServer.java
@@ -14,33 +14,36 @@ import java.io.*;
 import java.util.Date;
 
 public class DateServer{
-	public static void main(String[] args)  {
-		try {
+	public static void main(String[] args) throws Exception {
 			ServerSocket sock = new ServerSocket(6013);
 
 			System.out.println("=== Servidor iniciado ===\n");
 			// escutando por conexões
 			while (true) {
 				Socket client = sock.accept();
-				// Se chegou aqui, foi porque algum cliente se comunicou
-				System.out.println("Servidor recebeu comunicação do ip: " + client.getInetAddress() + "-" + client.getPort());
-				PrintWriter pout = new PrintWriter(client.getOutputStream(), true);
-
-				// Escreve a data atual no socket
-				pout.println(new Date().toString() + "-Boa noite alunos!");
-
-				InputStream in = client.getInputStream();
-				BufferedReader bin = new BufferedReader(new InputStreamReader(in));
-
-				String line = bin.readLine();
-				System.out.println("O cliente me disse:" + line);
-
-				// fechar o socket e volta no loop para escutar novas conexões
-				client.close();
+				Thread thread = new Thread(() -> {
+					try {
+						// Se chegou aqui, foi porque algum cliente se comunicou
+						System.out.println("Servidor recebeu comunicação do ip: " + client.getInetAddress() + "-" + client.getPort());
+						PrintWriter pout = new PrintWriter(client.getOutputStream(), true);
+	
+						// Escreve a data atual no socket
+						// Yaguinho mandou para Olívia
+						pout.println(new Date().toString() + " -> Olívia da Costa Oliva");
+	
+						InputStream in = client.getInputStream();
+						BufferedReader bin = new BufferedReader(new InputStreamReader(in));
+	
+						String line = bin.readLine();
+						System.out.println("O cliente me disse:" + line);
+	
+						// fechar o socket e volta no loop para escutar novas conexões
+						client.close();
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				});
+				thread.start();
 			}
 		}
-		catch (IOException ioe) {
-				System.err.println(ioe);
-		}
 	}
-}
